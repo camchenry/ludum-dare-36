b3796c10a82cfbf757c205913a65379b7fcd647e
Cameron McHenry <camchenry@users.noreply.github.com>
2016-08-27 00:23:12 -0400

    Added camera, sti, canvas

--- states/game.lua
+++ states/game.lua
@@ -1,11 +1,29 @@
 game = {}
 
-function game:enter()
-    
+sti = require "libs.sti"
+
+CANVAS_WIDTH = 240
+CANVAS_HEIGHT = 160
+
+function game:enter(from, ...)
+    self:reset() 
+end
+
+function game:reset()
+    self.map = sti("assets/maps/map01.lua", {"bump"}) 
+    self.canvas = love.graphics.newCanvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    SCALEX = love.graphics.getWidth() / CANVAS_WIDTH
+    SCALEY = love.graphics.getHeight() / CANVAS_HEIGHT
+
+    self.world = Bump.newWorld()
+    self.map:bump_init(self.world)
+
+    self.camera = Camera(0, 0)
 end
 
 function game:update(dt)
-
+    self.map:update(dt)
+    self.world:update(dt)
 end
 
 function game:keypressed(key, code)
@@ -17,5 +35,16 @@ function game:mousepressed(x, y, mbutton)
 end
 
 function game:draw()
+    self.canvas:renderTo(function()
+        love.graphics.clear()  
+        self.camera:attach()
+        self.map:setDrawRange(self.camera.x, self.camera.y, CANVAS_WIDTH, CANVAS_HEIGHT)
+        self.map:draw()
+        love.graphics.setColor(255, 255, 255)
+        love.graphics.circle("fill", 40, 40, 10)
+        self.camera:detach()
+    end)
 
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.draw(self.canvas, 0, 0, 0, SCALEX, SCALEY)
 end
