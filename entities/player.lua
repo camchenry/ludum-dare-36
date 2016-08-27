76d15509f92e2ede8a7dfca0896d322352dd412f
Nuthen <nuthen@comcast.net>
2016-08-27 01:03:24 -0400

    updated player class

--- entities/player.lua
+++ entities/player.lua
@@ -6,21 +6,33 @@ function Player:initialize(x, y)
 
     self.position = vector(x, y)
     self.velocity = vector(0, 0)
-    self.acceleration = vector(0, -9.8)
+    self.acceleration = vector(0, 0)
 
+    self.gravity = 9.8
     self.velMax = 20
-    self.accel = 2
+    self.moveAccel = 2
+    self.jumpAccel = 20
 end
 
 function Player:update(dt)
+    self.acceleration = vector(0, self.gravity)
+
+    if love.keyboard.isDown("w", "up") then
+        self.acceleration.y = self.acceleration.y - self.jumpAccel
+    end
+
     if love.keyboard.isDown("a", "left") then
-        self.acceleration.x = -self.accel
+        self.acceleration.x = -self.moveAccel
     elseif love.keyboard.isDown("d", "right") then
-        self.acceleration.x = self.accel
+        self.acceleration.x = self.moveAccel
     end
 
     self.velocity = self.velocty + self.acceleration * dt
     local actualX, actualY, cols, len = gameworld:move(self, self.position.x + self.velocty.x*dt, self.position.y + self.velocity.y*dt)
+
+    if #cols > 0 then
+        self.velocity = vector(0, 0)
+    end
 
     self.position = vector(actualX, actualY)
 end
