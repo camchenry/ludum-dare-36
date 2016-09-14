67c78eb72c833daf5aa89e170b35a593cb80035e
Cameron McHenry <camchenry@users.noreply.github.com>
2016-09-14 19:10:54 -0400

    Fixed a naming error

--- entities/transition.lua
+++ entities/transition.lua
@@ -1,13 +1,13 @@
 local Transition = Class("Transition")
 
 function Transition:initialize()
-    self.to = nil
+    self.toState = nil
 end
 
 function Transition:update(dt)
     if self.transitioning then
         if Fade:isActive() then
-            State.switch(self.to) 
+            State.switch(self.toState)
             Fade:unsubscribe("transition")
             self.transitioning = false
         end
@@ -18,7 +18,7 @@ function Transition:to(state)
     if type(state) == "string" then
         state = _G[state] 
     end
-    self.to = state 
+    self.toState = state 
 
     Fade:subscribe("transition")
     self.transitioning = true
